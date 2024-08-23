2ac48d0cd0b36842bfd051e1596afa7a1ee31f26
Daylen Jones <jonesday001@warren.k12.in.us>
2024-08-23 13:10:37 -0400

    update

---
+++ src/PTK.java
@@ -0,0 +1,9 @@
+public class PTK {
+    public static void main(String[] args) {
+        double pounds = 200;
+
+        double kilograms = pounds * 0.45359237;
+
+        System.out.println(pounds + " pounds is equal to " + kilograms + " kilograms ");
+    }
+}
